f9801b035e296fe9a7cbe19fd72f16260e57f6c6
SpikE-Odets <51069593+SpikE-Odets@users.noreply.github.com>
2019-05-27 00:38:04 -0400

    Add files via upload

---
+++ esx_mugging/main_c.lua
@@ -0,0 +1,234 @@
+ESX                           = nil
+lastrobbed = 0
+local robbing = false
+local currentrobbing = false
+local giveableItems = {
+    'weed_pooch',
+	'water',
+	'bread',
+	'lighter',
+	'coke_pooch',
+	'vodka',
+	'lsd_pooch',
+	'soda'
+}
+
+RegisterCommand('suicide', function()
+    SetEntityHealth(PlayerPedId(),0)
+end, false)
+
+Citizen.CreateThread(function()
+
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+    ESX.PlayerData = ESX.GetPlayerData()
+end)
+
+RegisterCommand('job', function()
+    ESX.PlayerData = ESX.GetPlayerData()
+    if ESX.PlayerData.job.name == 'police' then
+        ESX.ShowNotification(GetPlayerServerID(GetPlayerPed(-1)))
+    else
+       ESX.ShowNotification("knows im not a police")
+    end
+end, false)
+
+Citizen.CreateThread(function()
+    while true do
+        plyPos = GetEntityCoords(GetPlayerPed(-1),  true)
+        s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
+        street1 = GetStreetNameFromHashKey(s1)
+        street2 = GetStreetNameFromHashKey(s2)
+        Citizen.Wait(0)
+        if robbing then
+
+        else        
+         ESX.PlayerData = ESX.GetPlayerData()
+        local aiming, targetPed = GetEntityPlayerIsFreeAimingAt(PlayerId(-1))
+            if IsPedArmed(GetPlayerPed(-1), 7) and IsPedArmed(GetPlayerPed(-1), 4) and ESX.PlayerData.job.name ~= 'police'  then
+                if aiming then
+                local playerPed = GetPlayerPed(-1)
+                local pCoords = GetEntityCoords(playerPed, true)
+                local tCoords = GetEntityCoords(targetPed, true)
+                    if DoesEntityExist(targetPed) and IsEntityAPed(targetPed) and not IsPedDeadOrDying(targetPed) then
+                        SetEntityAsMissionEntity(targetPed, true, true)
+                        if GetDistanceBetweenCoords(pCoords.x, pCoords.y, pCoords.z, tCoords.x, tCoords.y, tCoords.z, true) <= 5.0 then
+                            if IsPedInAnyVehicle(targetPed, true) then
+                                local localvehicle = GetVehiclePedIsIn(targetPed, false)
+                                if IsVehicleStopped(localvehicle) then
+                                    TaskLeaveVehicle(targetPed, localvehicle, 1)
+                                    ClearPedTasks(targetPed)
+                                    Citizen.Wait(1000)
+                                    if not robbing then
+                                        robNpc(targetPed)
+                                    end
+                                end
+                            elseif not robbing then
+
+                                robNpc(targetPed)
+                            end
+                        end
+                    end    
+                end
+            end
+        end  
+    end
+end)
+
+function robNpc(targetPed)
+    Citizen.CreateThread(function()
+    local roblocalcoords = GetEntityCoords(targetPed)
+    if not currentrobbing then 
+    ESX.Game.Utils.DrawText3D(roblocalcoords, "[~g~E~s~] to Mug", 0.25)
+    elseif lasttargetPed == targetPed then
+    ESX.Game.Utils.DrawText3D(roblocalcoords, "Already Mugged..", 0.25)
+    else
+    ESX.Game.Utils.DrawText3D(roblocalcoords, "Mugging...", 0.25)
+    end
+    TaskHandsUp(targetPed, 5500, 0, 0, true)
+        if IsControlJustReleased(0, 38) then
+            if not currentrobbing then
+                if lasttargetPed == targetPed then
+                    PlayAmbientSpeech1(targetPed, "GUN_BEG", "SPEECH_PARAMS_FORCE_NORMAL_CLEAR")
+                    currentrobbing = true
+                    TaskHandsUp(targetPed, 1000, 0, 0, true)
+                    ESX.ShowNotification("Already Mugged this person.")
+                    TaskSmartFleePed(targetPed, GetPlayerPed(-1), -1, -1, true, true)
+                    SetEntityAsNoLongerNeeded(targetPed)
+                    Citizen.Wait(2000)
+                    robbing = false
+                    currentrobbing = false
+                else
+                    PlayAmbientSpeech1(targetPed, "GUN_BEG", "SPEECH_PARAMS_FORCE_NORMAL_CLEAR")
+                    currentrobbing = true
+                    TaskHandsUp(targetPed, 5500, 0, 0, true)
+                    Citizen.Wait(Config.RobWaitTime * 1000)
+                    SetEntityAsNoLongerNeeded(targetPed)
+                    if not IsPedFleeing(targetPed) then
+                       if not IsPedDeadOrDying(targetPed) then
+                            TriggerServerEvent("esx_muggings:giveMoney")
+                            additems = math.random(1,100)
+                                if additems <= Config.AddItemsPerctent then
+                                        randomitemcount = math.random(1,Config.AddItemsMax)
+                                    for i = randomitemcount,1,-1
+                                    do
+                                        local itemName = giveableItems[GetRandomIntInRange(1,  #giveableItems)]
+                                        TriggerServerEvent('esx_muggings:giveItems', (itemName))
+                                    end
+                                end
+                            randomact = math.random(1,10)
+                            if randomact > 6 then
+                                PlayAmbientSpeech1(targetPed, "GENERIC_INSULT_HIGH", "SPEECH_PARAMS_FORCE_NORMAL_CLEAR")
+                            elseif randomact > 3 then
+                                PlayAmbientSpeech1(targetPed, "GENERIC_FRIGHTENED_HIGH", "SPEECH_PARAMS_FORCE_NORMAL_CLEAR")
+                            end
+                            robbing = true
+                            lastrobbed = math.random(1, 100)
+                            if lastrobbed <= Config.PoliceNotify then
+                                ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+                                    local sex = nil
+                                    if skin.sex == 0 then
+                                        sex = "Male" --male/change it to your language
+                                    else
+                                        sex = "Female" --female/change it to your language
+                                    end
+                                    TriggerServerEvent('esx_mugging:muggingPos', plyPos.x, plyPos.y, plyPos.z)
+
+                                    if s2 == 0 then
+                                        TriggerServerEvent('esx_mugging:muggingAlertS1', street1, sex)
+                                    elseif s2 ~= 0 then
+                                        TriggerServerEvent('esx_mugging:muggingAlert', street1, street2, sex)
+                                    end
+                                end)
+                            end
+                            TriggerServerEvent('esx_mugging:muggingNotify')
+                            TaskSmartFleePed(targetPed, GetPlayerPed(-1), -1, -1, true, true)
+                            SetEntityAsNoLongerNeeded(targetPed)
+                            Citizen.Wait(2000)
+                            lasttargetPed = targetPed
+                            robbing = false
+                            currentrobbing = false
+                        else
+                            if Config.AlwaysNotifyonDeath then
+                                SetEntityAsNoLongerNeeded(targetPed)
+                                ESX.ShowNotification("Target died - Police will be notified")
+                                ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+                                    local sex = nil
+                                    if skin.sex == 0 then
+                                        sex = "Male" --male/change it to your language
+                                    else
+                                        sex = "Female" --female/change it to your language
+                                    end
+                                    TriggerServerEvent('esx_mugging:muggingPos', plyPos.x, plyPos.y, plyPos.z)
+                                    TriggerServerEvent('esx_mugging:muggingAlertS2', street1, sex)
+
+                                end)
+                            end
+                            robbing = false
+                            currentrobbing = false
+                        end
+                    else
+                        ESX.ShowNotification("Target ran away")
+                        SetEntityAsNoLongerNeeded(targetPed)
+                        lastrobbed = math.random(1, 100)
+                        if lastrobbed <= Config.PoliceNotify then
+                            ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
+                                local sex = nil
+                                if skin.sex == 0 then
+                                    sex = "Male" --male/change it to your language
+                                else
+                                    sex = "Female" --female/change it to your language
+                                end
+                                TriggerServerEvent('esx_mugging:muggingPos', plyPos.x, plyPos.y, plyPos.z)
+                                if s2 == 0 then
+                                    TriggerServerEvent('esx_mugging:muggingAlertS1', street1, sex)
+                                elseif s2 ~= 0 then
+                                    TriggerServerEvent('esx_mugging:muggingAlert', street1, street2, sex)
+                                end
+                            end)
+                        end
+                        robbing = false
+                        currentrobbing = false
+                    end
+                end
+            end
+        end
+    end)
+end
+
+GetPlayerName()
+RegisterNetEvent('muggingNotify')
+AddEventHandler('muggingNotify', function(alert, xPlayer)
+        if  ESX.PlayerData.job.name == 'police' then       
+        ESX.ShowAdvancedNotification('911 Emergency', 'Mugging', alert, 'CHAR_CALL911', 1)
+        end
+end)
+
+function Notify(text)
+    SetNotificationTextEntry('STRING')
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
+
+RegisterNetEvent('esx_mugging:muggingPos')
+AddEventHandler('esx_mugging:muggingPos', function(tx, ty, tz)
+	if ESX.PlayerData.job.name == 'police' then
+		local transT = 250
+		local Blip = AddBlipForCoord(tx, ty, tz)
+		SetBlipSprite(Blip,  10)
+		SetBlipColour(Blip,  1)
+		SetBlipAlpha(Blip,  transT)
+		SetBlipAsShortRange(Blip,  false)
+		while transT ~= 0 do
+			Wait(100)
+			transT = transT - 1
+			SetBlipAlpha(Blip,  transT)
+			if transT == 0 then
+				SetBlipSprite(Blip,  2)
+				return
+			end
+		end
+	end
+end)
---
+++ esx_mugging/main_s.lua
@@ -0,0 +1,49 @@
+local ESX = nil
+
+
+Citizen.CreateThread(function()
+
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+end)
+
+RegisterServerEvent('esx_muggings:giveMoney')
+AddEventHandler('esx_muggings:giveMoney', function()
+    local player = ESX.GetPlayerFromId(source)
+    local amount = math.random(Config.MinMoney, Config.MaxMoney)
+    player.addMoney(amount)
+    TriggerClientEvent("esx:showNotification", source, ("You stole $%s"):format(amount))
+end)
+
+RegisterServerEvent('esx_muggings:giveItems')
+AddEventHandler('esx_muggings:giveItems', function(itemName)
+    local _source = source
+	local xPlayer  = ESX.GetPlayerFromId(source)
+        xPlayer.addInventoryItem(itemName, 1)
+
+end)
+
+
+RegisterServerEvent('esx_mugging:muggingAlert')
+AddEventHandler('esx_mugging:muggingAlert', function(street1, street2, sex)
+	TriggerClientEvent("muggingNotify", -1, "~r~Reported Mugging by ~w~ "..sex.." ~r~near~w~  "..street1.." ~r~ and ~w~  "..street2)
+	--TriggerClientEvent("outlawNotify", -1, "~r~Sprzedarz dragów przez ~w~"..sex.." ~r~między ~w~"..street1.."~r~ a ~w~"..street2)
+end)
+--if you need you can translate it to your language too
+RegisterServerEvent('esx_mugging:muggingAlertS1')
+AddEventHandler('esx_mugging:muggingAlertS1', function(street1, sex)
+      TriggerClientEvent("muggingNotify", -1, "~r~Reported Mugging by ~w~ "..sex.." ~r~near~w~  "..street1)
+	--TriggerClientEvent("outlawNotify", -1, "~r~Sprzedarz dragów przez ~w~"..sex.." ~r~przy ulicy ~w~"..street1)
+end)
+RegisterServerEvent('esx_mugging:muggingAlertS2')
+AddEventHandler('esx_mugging:muggingAlertS2', function(street1, sex)
+      TriggerClientEvent("muggingNotify", -1, "~r~Reported Murder by ~w~ "..sex.." ~r~near~w~  "..street1)
+end)
+
+RegisterServerEvent('esx_mugging:muggingPos')
+AddEventHandler('esx_mugging:muggingPos', function(gx, gy, gz)
+	TriggerClientEvent('esx_mugging:muggingPos', -1, gx, gy, gz)
+end)
