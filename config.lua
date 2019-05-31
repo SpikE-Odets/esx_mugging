3663ce2528f2d06bc19c453c9e45a1a3d8638edd
SpikE-Odets <51069593+SpikE-Odets@users.noreply.github.com>
2019-05-30 22:59:08 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -10,7 +10,7 @@ Config.RobWaitTime = 5              --  Current seconds it takes to finish muggi
 Config.AddItemsPerctent = 20        --  This is percent the script will add items as well as cash from mugging.
 Config.AddItemsMax = 3              --  This is the max number of items you can receive from NPC from mugging - min is 1
 
-Config.PoliceNotify = 90            --  This is percent the police will get notified: example: 40 means 40% chance of notification.
+Config.PoliceNotify = 40            --  This is percent the police will get notified: example: 40 means 40% chance of notification.
 Config.AlwaysNotifyonDeath = true   --  This will set to 100% notify if NPC dies while someone is mugging them.
 
 Config.giveableItems = {
