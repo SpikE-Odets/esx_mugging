09a751b5090d3cbde30b051eec90c51349ffc7e8
SpikE-Odets <51069593+SpikE-Odets@users.noreply.github.com>
2019-05-30 22:43:08 -0400

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script {
+    "main_c.lua",
+    "config.lua"
+}
+
+server_script {
+    "main_s.lua",
+    "config.lua"
+}
+
